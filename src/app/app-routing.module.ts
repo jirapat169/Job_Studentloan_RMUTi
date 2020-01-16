13d10adb5f4e3884d824980e094e017af35b6d14
jirapat169 <james.jirapat@outlook.com>
2020-01-16 22:05:33 +0700

    Update Files Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,54 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NotfoundComponent } from "./components/notfound/notfound.component";
+import { StudentGuard } from "./guards/student.guard";
+import { TeacherGuard } from "./guards/teacher.guard";
+import { CommitteeGuard } from "./guards/committee.guard";
+import { AdminGuard } from "./guards/admin.guard";
+import { RegisterComponent } from "./components/register/register.component";
+import { LoginComponent } from "./components/login/login.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { IndexComponent } from "./components/index/index.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: IndexComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  {
+    path: "admin",
+    loadChildren: () =>
+      import("./components/admin/admin.module").then(m => m.AdminModule),
+    canActivate: [AdminGuard]
+  },
+  {
+    path: "committee",
+    loadChildren: () =>
+      import("./components/committee/committee.module").then(
+        m => m.CommitteeModule
+      ),
+    canActivate: [CommitteeGuard]
+  },
+  {
+    path: "teacher",
+    loadChildren: () =>
+      import("./components/teacher/teacher.module").then(m => m.TeacherModule),
+    canActivate: [TeacherGuard]
+  },
+  {
+    path: "student",
+    loadChildren: () =>
+      import("./components/student/student.module").then(m => m.StudentModule),
+    canActivate: [StudentGuard]
+  },
+  {
+    path: "**",
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { StudentGuard } from "./guards/student.guard";
+import { TeacherGuard } from "./guards/teacher.guard";
+import { CommitteeGuard } from "./guards/committee.guard";
+import { AdminGuard } from "./guards/admin.guard";
+import { HttpService } from "./services/http.service";
+import { AppService } from "./services/app.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SharedModules } from "./shared-modules";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { IndexComponent } from "./components/index/index.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    IndexComponent,
+    ContactComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModules
   ],
-  providers: [],
+  providers: [
+    AppService,
+    HttpService,
+    AdminGuard,
+    CommitteeGuard,
+    TeacherGuard,
+    StudentGuard
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
