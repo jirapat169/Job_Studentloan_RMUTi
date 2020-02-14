bc78e70154e55aab0de9d401cb151506a327fdfb
jirapat169 <james.jirapat@outlook.com>
2020-02-14 23:05:42 +0700

    Update View STD101

--- src/app/shared/view-term1/std101/non-parent/non-parent.component.ts
+++ src/app/shared/view-term1/std101/non-parent/non-parent.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { AppService } from "src/app/services/app.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-non-parent-std',
-  templateUrl: './non-parent.component.html',
-  styleUrls: ['./non-parent.component.scss']
+  selector: "app-non-parent-std",
+  templateUrl: "./non-parent.component.html",
+  styleUrls: ["./non-parent.component.scss"]
 })
 export class NonParentComponent implements OnInit {
-  @Input("route") route: string;
-  constructor() { }
+  @Input("route") route: any;
+  public pageWaitting: boolean = false;
+  public nonParent: any = {};
 
-  ngOnInit() {
-    console.log(this.route);
+  constructor(
+    private formBuilder: FormBuilder,
+    public service: AppService,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    this.pageWaitting = true;
+    await this.getNonParent();
+
+    console.log(this.nonParent);
+    this.pageWaitting = false;
   }
 
+  private getNonParent = async () => {
+    let http: any = await this.service.http.get(
+      `101_parent/get/${this.route.username}`
+    );
+
+    if (http.result.length > 0) {
+      Object.keys(http.result[0]).forEach(key => {
+        this.nonParent[`${key}`] = http.result[0][key];
+      });
+    }else{
+      this.nonParent == null;
+    }
+  };
 }
--- src/app/shared/view-term1/std101/spouse/spouse.component.ts
+++ src/app/shared/view-term1/std101/spouse/spouse.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { AppService } from "src/app/services/app.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-spouse-std",
@@ -6,10 +9,35 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./spouse.component.scss"]
 })
 export class SpouseComponent implements OnInit {
-  @Input("route") route: string;
-  constructor() {}
+  @Input("route") route: any;
+  public pageWaitting: boolean = false;
+  public spouse: any = {};
 
-  ngOnInit() {
-    console.log(this.route);
+  constructor(
+    private formBuilder: FormBuilder,
+    public service: AppService,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    this.pageWaitting = true;
+    await this.getSpouse();
+
+    console.log(this.spouse)
+    this.pageWaitting = false;
   }
+
+  private getSpouse = async () => {
+    let http: any = await this.service.http.get(
+      `101_spouse/get/${this.route.username}`
+    );
+
+    if (http.result.length > 0) {
+      Object.keys(http.result[0]).forEach(key => {
+        this.spouse[`${key}`] = http.result[0][key];
+      });
+    } else {
+      this.spouse = null;
+    }
+  };
 }
